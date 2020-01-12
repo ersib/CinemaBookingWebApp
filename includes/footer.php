dec0134232e5857824a250e11b3ea7a3dc981fe3
ersib <57100291+ersib@users.noreply.github.com>
2020-01-12 13:26:03 +0100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<div id="footer">
+  <div>
+    <p>
+      &#169; 2020 Cinema Theatre
+    </p>
+    <div class="connect">
+      <span>Stay Connected:</span> <a href="http://freewebsitetemplates.com/go/facebook/" id="facebook">facebook</a> <a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a> <a href="http://freewebsitetemplates.com/go/googleplus/" id="googleplus">google+</a>
+    </div>
+  </div>
+</div>
---
+++ includes/headers.php
@@ -0,0 +1,27 @@
+<div id="header">
+  <div>
+    <a href="index.php" id="logo"><img src="images/logo.png" alt=""></a>
+    <ul>
+      <li class="selected">
+        <a href="index.php">Home</a>
+      </li>
+      <li>
+        <a href="movies.php">Movies</a>
+      </li>
+      <li>
+        <a href="bookticket.php">Book Ticket</a>
+      </li>
+      <li>
+        <a href="registerform.php">Register</a>
+      </li>
+      <li>
+        <a href="loginform.php">Login</a>
+      </li>
+      <li>
+        <a href="contactus.php">Contact Us</a>
+      </li>
+
+    </ul>
+  </div>
+
+</div>
