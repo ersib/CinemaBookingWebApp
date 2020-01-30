85195c97625c805b172b596dd077da74a4ce8de1
ersib <57100291+ersib@users.noreply.github.com>
2020-01-30 15:13:00 +0100

    Add files via upload

---
+++ kontrollo_logim.php
@@ -0,0 +1,38 @@
+<?php
+require 'classes/user.php';
+if(isset($_POST['submit']))
+{
+   $username=$_POST['username'];
+   $password=$_POST['password'];
+
+   $user=new User();
+
+  if($user->authenticateAdmin($username,$password)){
+    session_start();
+    $_SESSION['admin'] = $username;
+    header( "location: admin/admin_board.php");
+    }
+
+   if( $userId=$user->authenticateUser($username,$password)){
+     session_start();
+     $_SESSION['iduser']=$userId;
+     $_SESSION['username'] = $username;
+     $_SESSION['password'] = $password;
+     header( "location: user/Uboard.php");
+  }
+  else {
+    ?>
+    <script type="text/javascript">
+      alert("Incorrect credentials ! Please try again");
+      window.location.href="logimi.php";
+    </script>
+    <?php
+
+  }
+}
+if(isset($_POST['register']))
+{
+  header( "location: regjistrimi.php");
+}
+
+?>
